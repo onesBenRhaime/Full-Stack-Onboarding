8befe2bc8fc4f3f318bc632168b7df44834ff24d
onesBenRhaime <onesrhaime28@gmail.com>
2024-08-16 23:07:08 +0100

    Step 1  : backend

---
+++ backend/src/user/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Role } from 'src/role/entities/role.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @ManyToMany(() => Role)
+  @JoinTable()
+  roles: Role[];
+}
