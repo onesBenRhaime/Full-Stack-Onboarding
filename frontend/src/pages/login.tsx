095ab4a5e08205cf0fd02ca96a67d8e1a6d3e12a
onesBenRhaime <onesrhaime28@gmail.com>
2024-08-17 17:20:09 +0100

    adding tailwindcss

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,9 +3,12 @@ import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
 
 const schema = Yup.object({
-	username: Yup.string().required("Username is required"),
+	email: Yup.string()
+		.email("Invalid email format")
+		.required("Email is required"),
 	password: Yup.string().required("Password is required"),
 });
 
@@ -35,22 +38,100 @@ const LoginPage = () => {
 	};
 
 	return (
-		<div className="container mx-auto p-4">
-			<h1 className="text-2xl font-bold mb-4">Login</h1>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<div>
-					<label htmlFor="username">Username</label>
-					<input id="username" {...register("username")} />
-					{errors.username && <p>{errors.username.message}</p>}
+		<>
+			<header className="w-full py-4  mt-5 text-black">
+				<div className="container mx-auto flex justify-between items-center">
+					<div className="text-2xl font-bold">E-commerce-Fullstack</div>
+					<nav>
+						<ul className="flex space-x-4">
+							<li>
+								<Link href="/login" className="hover:underline">
+									Home
+								</Link>
+							</li>
+							<li>
+								<Link href="/login" className="hover:underline">
+									Login
+								</Link>
+							</li>
+							<li>
+								<Link href="/register" className="hover:underline">
+									Register
+								</Link>
+							</li>
+						</ul>
+					</nav>
 				</div>
-				<div>
-					<label htmlFor="password">Password</label>
-					<input id="password" type="password" {...register("password")} />
-					{errors.password && <p>{errors.password.message}</p>}
+			</header>
+			<section className="relative z-10 overflow-hidden pb-16 pt-36 md:pb-20 lg:pb-28 lg:pt-[8 0px]">
+				<div className="container">
+					<div className="-mx-4 flex flex-wrap">
+						<div className="w-full px-4">
+							<div className="shadow-three mx-auto max-w-[500px] rounded bg-white px-6 py-10 dark:bg-dark sm:p-[60px]">
+								<div className="mb-8 flex items-center justify-center">
+									<span className="hidden h-[1px] w-full max-w-[70px] bg-body-color/50 sm:block"></span>
+									<p className="w-full px-5 text-center text-base font-medium text-body-color">
+										Login
+									</p>
+									<span className="hidden h-[1px] w-full max-w-[70px] bg-body-color/50 sm:block"></span>
+								</div>
+								<form onSubmit={handleSubmit(onSubmit)}>
+									<div className="mb-8">
+										<label
+											htmlFor="email"
+											className="mb-3 block text-sm text-dark dark:text-white"
+										>
+											Your Email
+										</label>
+										<input
+											type="email"
+											id="email"
+											{...register("email")}
+											placeholder="Enter your Email"
+											className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
+										/>
+										{errors.email && (
+											<p className="text-red-500">{errors.email.message}</p>
+										)}
+									</div>
+									<div className="mb-8">
+										<label
+											htmlFor="password"
+											className="mb-3 block text-sm text-dark dark:text-white"
+										>
+											Your Password
+										</label>
+										<input
+											type="password"
+											id="password"
+											{...register("password")}
+											placeholder="Enter your Password"
+											className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
+										/>
+										{errors.password && (
+											<p className="text-red-500">{errors.password.message}</p>
+										)}
+									</div>
+									<button
+										type="submit"
+										className="w-full bg-primary text-white rounded-sm py-3"
+									>
+										Login
+									</button>
+								</form>
+								<div className="mb-8  flex items-center justify-center">
+									<span className="hidden h-[1px] w-full max-w-[70px] bg-body-color/50 sm:block"></span>
+									<p className="w-full px-5 text-center text-base font-medium text-body-color">
+										or Register
+									</p>
+									<span className="hidden h-[1px] w-full max-w-[70px] bg-body-color/50 sm:block"></span>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<button type="submit">Login</button>
-			</form>
-		</div>
+			</section>
+		</>
 	);
 };
 
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,88 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+module.exports = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	darkMode: "class",
+	theme: {
+		container: {
+			center: true,
+			padding: "1rem",
+		},
+		containerPolicy: {
+			center: true,
+			padding: "5rem",
+		},
+		screens: {
+			xs: "450px",
+			sm: "575px",
+			md: "768px",
+			lg: "992px",
+			xl: "1200px",
+			"2xl": "1400px",
+		},
+		extend: {
+			fontFamily: {
+				sans: ['"DM Sans"', "sans-serif"],
+			},
+			lineHeight: {
+				64: "16rem",
+			},
+			colors: {
+				linear: "#c6d1ed",
+				current: "currentColor",
+				transparent: "transparent",
+				white: "#FFFFFF",
+				black: "#121723",
+				dark: "#1D2430",
+				primary: "#8E54E9",
+				secondary: "#E8456B",
+				yellow: "#FBB040",
+				neutral: "#14142B",
+				danger: "red",
+
+				"bg-color-dark": "#171C28",
+				"body-color": {
+					DEFAULT: "#788293",
+					dark: "#959CB1",
+				},
+				stroke: {
+					stroke: "#E3E8EF",
+					dark: "#353943",
+				},
+				grayCookies: "#6F6C90",
+				gray: {
+					...colors.gray,
+					dark: "#1E232E",
+					light: "#F0F2F9",
+				},
+				"custom-blue": "#3A6AE5",
+				"custom-black": "#000000",
+			},
+			boxShadow: {
+				one: "0px 2px 3px rgba(7, 7, 77, 0.05)",
+				two: "0px 5px 10px rgba(6, 8, 15, 0.1)",
+				three: "0px 5px 15px rgba(6, 8, 15, 0.05)",
+				sticky: "inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)",
+				"sticky-dark": "inset 0 -1px 0 0 rgba(255, 255, 255, 0.1)",
+				"feature-2": "0px 10px 40px rgba(48, 86, 211, 0.12)",
+				submit: "0px 5px 20px rgba(4, 10, 34, 0.1)",
+				"submit-dark": "0px 5px 20px rgba(4, 10, 34, 0.1)",
+				btn: "0px 1px 2px rgba(4, 10, 34, 0.15)",
+				"btn-hover": "0px 1px 2px rgba(0, 0, 0, 0.15)",
+				"btn-light": "0px 1px 2px rgba(0, 0, 0, 0.1)",
+			},
+			dropShadow: {
+				three: "0px 5px 15px rgba(6, 8, 15, 0.05)",
+			},
+			background: {
+				"custom-gradient": "#c6d1ed",
+			},
+		},
+	},
+	plugins: [],
 };
-export default config;
