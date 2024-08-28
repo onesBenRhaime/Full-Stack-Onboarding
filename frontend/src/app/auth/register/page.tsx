753e7e47db0081488c357c28e05628c9a7ecf43f
onesBenRhaime <onesrhaime28@gmail.com>
2024-08-28 03:15:24 +0100

    login

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -101,18 +101,9 @@ export default function Register() {
 
 	return (
 		<>
-			<section className="relative flex flex-col md:flex-row justify-between items-center">
-				<div className="w-full md:w-auto">
-					{alertMessage && (
-						<div className="fixed bottom-4 top-0  right-4  ">
-							<Toast
-								title={alertMessage.title}
-								description={alertMessage.description}
-								variant={alertMessage.variant}
-							/>
-						</div>
-					)}
-					<h1 className=" fixed left-20 top-8 z-0  font-bold text-2xl">
+			<section className="flex flex-col lg:flex-row justify-center lg:justify-start items-center fixed">
+				<div className="w-full lg:w-auto lg:h-auto h-full flex justify-center items-center">
+					<h1 className=" fixed left-20 top-8 z-0  font-bold text-5xl">
 						<span className="text-primary ">E-</span>commerce
 					</h1>
 					<Image
@@ -120,21 +111,130 @@ export default function Register() {
 						height="1024"
 						alt="Register"
 						src="/images/register.png"
-						className="w-full h-auto md:h-full object-cover"
+						className="w-full lg:w-auto lg:block hidden"
 					/>
 				</div>
-				<div className="w-full md:w-1/3 lg:w-1/2 lg:m-8 p-4">
-					<div className="">
-						<h1 className="font-bold text-2xl md:text-3xl text-black">
+				<div className="lg:w-1/2 w-full p-4 lg:p-0 flex-row  items-center justify-center   lg:static absolute inset-0 lg:mt-0 mt-0 mb-24  max-w-full">
+					{alertMessage && (
+						<div className="fixed bottom-4 lg:top-0 lg:right-4 right-4">
+							<Toast
+								title={alertMessage.title}
+								description={alertMessage.description}
+								variant={alertMessage.variant}
+							/>
+						</div>
+					)}
+					<div className="bg-white border rounded-xl px-10 py-4 w-full lg:w-auto mx-4">
+						<h1 className="font-bold text-5xl text-black">
 							Create New Account
 						</h1>
-						<p className="text-gray-500 text-lg md:text-xl font-sans py-4">
+						<p className="text-gray-500 text-xl font-sans py-4">
 							Please enter details
 						</p>
-						<div>
-							{/* Form */}
 
-							<form onSubmit={onSubmit}>
+						<form onSubmit={onSubmit} className="">
+							<div className="mb-4">
+								<label
+									htmlFor="username"
+									className="mb-3 block text-sm text-dark font-sans"
+								>
+									UserName
+								</label>
+								<input
+									type="text"
+									id="username"
+									placeholder="Enter your UserName"
+									className="w-full rounded-md bg-white px-6 py-3 text-base outline-none transition-all duration-300 w-"
+									style={{
+										border: "1px solid #131118",
+										borderRadius: "10px",
+									}}
+									onChange={(e) => setUsername(e.target.value)}
+								/>
+								{errors.username && (
+									<p className="text-red-500">{errors.username}</p>
+								)}
+							</div>
+							<div className="mb-8">
+								<label
+									htmlFor="email"
+									className="mb-3 block text-sm text-dark font-sans"
+								>
+									Email Address
+								</label>
+								<input
+									type="email"
+									id="email"
+									onChange={(e) => setEmail(e.target.value)}
+									placeholder="Enter your Email Address"
+									className="w-full rounded-md bg-white px-6 py-3 text-base outline-none transition-all duration-300"
+									style={{
+										border: "1px solid #131118",
+										borderRadius: "10px",
+									}}
+								/>
+							</div>
+							{errors.email && <p className="text-red-500">{errors.email}</p>}
+							<div className="mb-4">
+								<label
+									htmlFor="password"
+									className="mb-3 block text-sm text-dark font-sans"
+								>
+									Password
+								</label>
+								<input
+									type="password"
+									id="password"
+									onChange={(e) => setPassword(e.target.value)}
+									placeholder="Enter your Password"
+									className="w-full rounded-md bg-white px-6 py-3 text-base text-black outline-none transition-all duration-300"
+									style={{
+										border: "1px solid #131118",
+										borderRadius: "10px",
+									}}
+								/>
+								{errors.password && (
+									<p className="text-red-500">{errors.password}</p>
+								)}
+							</div>
+							<div className="mb-4 flex items-center">
+								<input
+									type="checkbox"
+									className="rounded-md bg-white me-2 accent-black"
+								/>
+								<span>
+									I agree to the <b>Terms & Conditions</b>
+								</span>
+							</div>
+							<div className="flex mb-2 justify-center">
+								<span>Already have an account?</span>
+								<Link
+									href="/auth/login"
+									className="ms-1 underline hover:text-primary hover:font-bold"
+								>
+									Login
+								</Link>
+							</div>
+							<button
+								type="submit"
+								className="w-full bg-black text-white rounded-sm py-3"
+							>
+								Sign Up
+							</button>
+						</form>
+					</div>{" "}
+				</div>
+				{/*	<div className="lg:w-1/2 w-full p-4 lg:p-0 flex items-center  lg:static absolute inset-0 lg:mt-40 mt-0   max-w-full">
+					<div className="bg-white border rounded-xl px-10 py-8 w-full lg:w-auto mx-4">
+						<div className="">
+							<h1 className="font-bold text-3xl text-black">
+								Create New Account
+							</h1>
+							<p className="text-gray-500 text-xl font-sans py-4">
+								Please enter details
+							</p>
+
+							<form onSubmit={onSubmit} className="">
 								<div className="mb-8">
 									<label
 										htmlFor="username"
@@ -146,13 +246,13 @@ export default function Register() {
 										type="text"
 										id="username"
 										placeholder="Enter your UserName"
-										className="w-full rounded-md bg-white px-6 py-3 text-base outline-none transition-all duration-300"
+										className="w-full rounded-md bg-white px-6 py-3 text-base outline-none transition-all duration-300 w-"
 										style={{
 											border: "1px solid #131118",
 											borderRadius: "10px",
 										}}
 										onChange={(e) => setUsername(e.target.value)}
-									/>{" "}
+									/>
 									{errors.username && (
 										<p className="text-red-500">{errors.username}</p>
 									)}
@@ -196,7 +296,7 @@ export default function Register() {
 										}}
 									/>
 									{errors.password && (
-										<p className="text-red-500">{errors.email}</p>
+										<p className="text-red-500">{errors.password}</p>
 									)}
 								</div>
 								<div className="mb-4 flex items-center">
@@ -226,7 +326,7 @@ export default function Register() {
 							</form>
 						</div>
 					</div>
-				</div>
+				</div>*/}
 			</section>
 		</>
 	);
