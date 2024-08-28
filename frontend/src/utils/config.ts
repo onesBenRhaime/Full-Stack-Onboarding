1329fa63782ec6481461d8f0b0b637765e9c585d
onesBenRhaime <onesrhaime28@gmail.com>
2024-08-28 03:47:08 +0100

    back + front

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,4 +1,4 @@
 // let API_BASE_URL = "https://full-stack-onboarding-back.vercel.app/";
-const API_BASE_URL = "http://localhost:5000/";
+const API_BASE_URL = "https://full-stack-backend-9r7p.onrender.com/";
 
 export default API_BASE_URL;
