6fe2ed58be6fe728164df93811cc9cc9ca712709
onesBenRhaime <onesrhaime28@gmail.com>
2024-08-28 09:51:45 +0100

    reg res

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -101,10 +101,10 @@ export default function Register() {
 
 	return (
 		<>
-			<section className="flex flex-col lg:flex-row justify-center lg:justify-start items-center fixed">
-				<div className="w-full lg:w-auto lg:h-auto h-full flex justify-center items-center">
+			<section className="flex flex-col lg:flex-row lg:justify-start justify-center lg:items-start items-center lg:space-x-0 space-x-2 lg:space-y-32 space-y-2 fixed inset-0 lg:static">
+				<div className="lg:w-auto w-full lg:h-auto h-full flex justify-center items-center">
 					<h1 className=" fixed left-20 top-8 z-0  font-bold text-5xl">
-						<span className="text-primary ">E-</span>commerce
+						<span className="text-primary  ">E-</span>commerce
 					</h1>
 					<Image
 						width="945"
@@ -114,7 +114,10 @@ export default function Register() {
 						className="w-full lg:w-auto lg:block hidden"
 					/>
 				</div>
-				<div className="lg:w-1/2 w-full p-4 lg:p-0 flex-row  items-center justify-center   lg:static absolute inset-0 lg:mt-0 mt-0 mb-24  max-w-full">
+				<div
+					className="lg:w-1/3 w-full p-4 lg:p-0 flex items-center justify-center lg:static absolute inset-0 lg:mt-32 mb-10 lg:mb-10  "
+					style={{ marginBottom: "10px" }}
+				>
 					{alertMessage && (
 						<div className="fixed bottom-4 lg:top-0 lg:right-4 right-4">
 							<Toast
@@ -124,15 +127,18 @@ export default function Register() {
 							/>
 						</div>
 					)}
-					<div className="bg-white border rounded-xl px-10 py-4 w-full lg:w-auto mx-4">
-						<h1 className="font-bold text-5xl text-black">
+					<div className="bg-white border rounded-xl px-5 py-8 w-full lg:w-auto">
+						<h1
+							className="font-bold text-3xl text-black "
+							style={{ fontSize: "45px", lineHeight: "1.2" }}
+						>
 							Create New Account
 						</h1>
 						<p className="text-gray-500 text-xl font-sans py-4">
 							Please enter details
 						</p>
 
-						<form onSubmit={onSubmit} className="">
+						<form onSubmit={onSubmit}>
 							<div className="mb-4">
 								<label
 									htmlFor="username"
@@ -224,109 +230,6 @@ export default function Register() {
 						</form>
 					</div>{" "}
 				</div>
-				{/*	<div className="lg:w-1/2 w-full p-4 lg:p-0 flex items-center  lg:static absolute inset-0 lg:mt-40 mt-0   max-w-full">
-					<div className="bg-white border rounded-xl px-10 py-8 w-full lg:w-auto mx-4">
-						<div className="">
-							<h1 className="font-bold text-3xl text-black">
-								Create New Account
-							</h1>
-							<p className="text-gray-500 text-xl font-sans py-4">
-								Please enter details
-							</p>
-
-							<form onSubmit={onSubmit} className="">
-								<div className="mb-8">
-									<label
-										htmlFor="username"
-										className="mb-3 block text-sm text-dark font-sans"
-									>
-										UserName
-									</label>
-									<input
-										type="text"
-										id="username"
-										placeholder="Enter your UserName"
-										className="w-full rounded-md bg-white px-6 py-3 text-base outline-none transition-all duration-300 w-"
-										style={{
-											border: "1px solid #131118",
-											borderRadius: "10px",
-										}}
-										onChange={(e) => setUsername(e.target.value)}
-									/>
-									{errors.username && (
-										<p className="text-red-500">{errors.username}</p>
-									)}
-								</div>
-								<div className="mb-8">
-									<label
-										htmlFor="email"
-										className="mb-3 block text-sm text-dark font-sans"
-									>
-										Email Address
-									</label>
-									<input
-										type="email"
-										id="email"
-										onChange={(e) => setEmail(e.target.value)}
-										placeholder="Enter your Email Address"
-										className="w-full rounded-md bg-white px-6 py-3 text-base outline-none transition-all duration-300"
-										style={{
-											border: "1px solid #131118",
-											borderRadius: "10px",
-										}}
-									/>
-								</div>
-								{errors.email && <p className="text-red-500">{errors.email}</p>}
-								<div className="mb-4">
-									<label
-										htmlFor="password"
-										className="mb-3 block text-sm text-dark font-sans"
-									>
-										Password
-									</label>
-									<input
-										type="password"
-										id="password"
-										onChange={(e) => setPassword(e.target.value)}
-										placeholder="Enter your Password"
-										className="w-full rounded-md bg-white px-6 py-3 text-base text-black outline-none transition-all duration-300"
-										style={{
-											border: "1px solid #131118",
-											borderRadius: "10px",
-										}}
-									/>
-									{errors.password && (
-										<p className="text-red-500">{errors.password}</p>
-									)}
-								</div>
-								<div className="mb-4 flex items-center">
-									<input
-										type="checkbox"
-										className="rounded-md bg-white me-2 accent-black"
-									/>
-									<span>
-										I agree to the <b>Terms & Conditions</b>
-									</span>
-								</div>
-								<div className="flex mb-2 justify-center">
-									<span>Already have an account?</span>
-									<Link
-										href="/auth/login"
-										className="ms-1 underline hover:text-primary hover:font-bold"
-									>
-										Login
-									</Link>
-								</div>
-								<button
-									type="submit"
-									className="w-full bg-black text-white rounded-sm py-3"
-								>
-									Sign Up
-								</button>
-							</form>
-						</div>
-					</div>
-				</div>*/}
 			</section>
 		</>
 	);
--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,4 +1,5 @@
-// let API_BASE_URL = "https://full-stack-onboarding-back.vercel.app/";
 const API_BASE_URL = "https://full-stack-backend-9r7p.onrender.com/";
+
+// const API_BASE_URL = "http://localhost:5000/";
 
 export default API_BASE_URL;
