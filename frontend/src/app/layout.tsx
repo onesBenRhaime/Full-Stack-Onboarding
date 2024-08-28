0f0ddd0c8a38fbdbcf9a85babae692df88388c32
onesBenRhaime <onesrhaime28@gmail.com>
2024-08-28 03:25:35 +0100

    back

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -11,6 +11,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning={true}>
+			<head>
+				<meta charSet="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+				<meta name="description" content="A Next.js app with Tailwind CSS" />
+				<title>E-commerce</title>
+				<link rel="icon" href="/images/icon.png" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+			</head>
 			<body className={recursive.className} suppressHydrationWarning={true}>
 				<main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
 					<div className="flex-1 flex flex-col h-full">
